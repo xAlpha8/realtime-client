651470e8b301d53d080cc9bba0c2f566a0adb425
Janak <janak2@users.noreply.github.com>
2024-07-29 21:07:17 -0700

    disable sentry locally

--- realtime/__init__.py
+++ realtime/__init__.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 if sys.version_info[:2] < (3, 9):
@@ -7,22 +8,24 @@ if sys.version_info[:2] >= (3, 13):
 
 import sentry_sdk
 
-sentry_sdk.init(
-    dsn="https://8842715aaa1b7fd845f8a55eea150394@o4506805333983232.ingest.us.sentry.io/4507603326795776",
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
-)
+if os.getenv("PRODUCTION_ENVIRONMENT", False):
+    sentry_sdk.init(
+        dsn="https://8842715aaa1b7fd845f8a55eea150394@o4506805333983232.ingest.us.sentry.io/4507603326795776",
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+    )
 
 try:
     from .app import App  # noqa: F401
     from .function import function  # noqa: F401
     from .streaming_endpoint import streaming_endpoint  # noqa: F401
     from .web_endpoint import web_endpoint  # noqa: F401
+    from .websocket import websocket  # noqa: F401
 except Exception:
     print()
     print("#" * 50)
@@ -31,4 +34,4 @@ except Exception:
     print()
     raise
 
-__all__ = ["function", "streaming_endpoint", "App", "web_endpoint"]
+__all__ = ["function", "streaming_endpoint", "App", "web_endpoint", "websocket"]

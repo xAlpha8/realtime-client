7a858b07a8fcb3d30ec5ad8669dbfc8075cd32ed
Janak <janak2@users.noreply.github.com>
2024-08-11 00:20:16 -0700

    add check for correct input

--- realtime/streams.py
+++ realtime/streams.py
@@ -1,23 +1,15 @@
 import asyncio
 from typing import List
-
+import uuid
+from realtime.data import AudioData, ImageData, TextData, ByteData
 
 class Stream(asyncio.Queue):
     """An asynchronous queue where objects added to it are also added to its copies."""
 
-    def __init__(self):
+    def __init__(self, id: str = None):
         super().__init__()
+        self.id = id or str(uuid.uuid4())
         self._clones: List[Stream] = []
-
-    async def put(self, item):
-        """Put an item in all queues of all instances."""
-        self.put_nowait(item)
-
-    def put_nowait(self, item):
-        """Put an item in all queues of all instances."""
-        super().put_nowait(item)
-        for clone in self._clones:
-            clone.put_nowait(item)
 
 
 class AudioStream(Stream):
@@ -29,6 +21,17 @@ class AudioStream(Stream):
         self._clones.append(clone)
         return clone
 
+    async def put(self, item):
+        """Put an item in all queues of all instances."""
+        self.put_nowait(item)
+
+    def put_nowait(self, item):
+        """Put an item in all queues of all instances."""
+        if type(item) != AudioData:
+            raise ValueError("AudioStream can only accept AudioData")
+        super().put_nowait(item)
+        for clone in self._clones:
+            clone.put_nowait(item)
 
 class VideoStream(Stream):
     type = "video"
@@ -38,6 +41,18 @@ class VideoStream(Stream):
         clone = VideoStream()
         self._clones.append(clone)
         return clone
+
+    async def put(self, item):
+        """Put an item in all queues of all instances."""
+        self.put_nowait(item)
+
+    def put_nowait(self, item):
+        """Put an item in all queues of all instances."""
+        if type(item) != ImageData:
+            raise ValueError("VideoStream can only accept ImageData")
+        super().put_nowait(item)
+        for clone in self._clones:
+            clone.put_nowait(item)
 
 
 class TextStream(Stream):
@@ -49,6 +64,18 @@ class TextStream(Stream):
         self._clones.append(clone)
         return clone
 
+    async def put(self, item):
+        """Put an item in all queues of all instances."""
+        self.put_nowait(item)
+
+    def put_nowait(self, item):
+        """Put an item in all queues of all instances."""
+        if type(item) != TextData:
+            raise ValueError("TextStream can only accept TextData")
+        super().put_nowait(item)
+        for clone in self._clones:
+            clone.put_nowait(item)
+
 
 class ByteStream(Stream):
     type = "bytes"
@@ -58,3 +85,15 @@ class ByteStream(Stream):
         clone = ByteStream()
         self._clones.append(clone)
         return clone
+
+    async def put(self, item):
+        """Put an item in all queues of all instances."""
+        self.put_nowait(item)
+
+    def put_nowait(self, item):
+        """Put an item in all queues of all instances."""
+        if type(item) != ByteData:
+            raise ValueError("ByteStream can only accept ByteData")
+        super().put_nowait(item)
+        for clone in self._clones:
+            clone.put_nowait(item)

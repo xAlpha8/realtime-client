589c2e3e69e7ba3d519237b2be3ab47648439617
Janak <janak2@users.noreply.github.com>
2024-08-21 19:27:29 -0700

    Update streams.py

--- realtime/streams.py
+++ realtime/streams.py
@@ -23,6 +23,10 @@ class Stream(asyncio.Queue):
 class AudioStream(Stream):
     type = "audio"
 
+    def __init__(self, sample_rate: int = 8000):
+        super().__init__()
+        self.sample_rate = sample_rate
+
     async def clone(self):
         """Create a copy of this queue."""
         clone = AudioStream()

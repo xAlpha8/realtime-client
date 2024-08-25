a9ed2483ce0534670520f7829bee32089e162b7d
Janak <janak2@users.noreply.github.com>
2024-08-25 16:55:25 -0700

    Update processors.py

--- realtime/websocket/processors.py
+++ realtime/websocket/processors.py
@@ -1,16 +1,15 @@
 import asyncio
 import base64
-import functools
 import io
-import json
 import logging
-import wave
-import numpy as np
-from realtime.server import RealtimeServer
-from fastapi import WebSocket
-from realtime.streams import AudioStream, TextStream, VideoStream, ByteStream
-import av
 import time
+import wave
+
+import av
+import numpy as np
+from fastapi import WebSocket
+
+from realtime.streams import AudioStream, ByteStream, TextStream, VideoStream
 
 
 class WebsocketInputStream:
@@ -22,6 +21,7 @@ class WebsocketInputStream:
         audio_output_stream (AudioStream): Stream for audio data.
         message_stream (TextStream): Stream for text messages.
     """
+
     def __init__(self, ws: WebSocket):
         self.ws = ws
 
@@ -49,9 +49,11 @@ class WebsocketInputStream:
                     if len(audio_data) < 2:
                         continue
                     if len(audio_data) % 2 != 0:
+                        # TODO: Get audio dtype from the frontend instead of hardcoding it
                         array = np.frombuffer(audio_data[:-1], dtype=np.int16).reshape(1, -1)
                         audio_data = audio_data[-1:]
                     else:
+                        # TODO: Get audio dtype from the frontend instead of hardcoding it
                         array = np.frombuffer(audio_data, dtype=np.int16).reshape(1, -1)
                         audio_data = b""
 
@@ -62,6 +64,7 @@ class WebsocketInputStream:
                 logging.error("websocket: Exception", e)
                 raise asyncio.CancelledError()
 
+
 class WebsocketOutputStream:
     """
     Handles outgoing WebSocket messages by streaming audio and text data.
@@ -69,10 +72,13 @@ class WebsocketOutputStream:
     Attributes:
         ws (WebSocket): The WebSocket connection.
     """
+
     def __init__(self, ws: WebSocket):
         self.ws = ws
 
-    async def run(self, audio_stream: AudioStream, message_stream: TextStream, video_stream: VideoStream, byte_stream: ByteStream):
+    async def run(
+        self, audio_stream: AudioStream, message_stream: TextStream, video_stream: VideoStream, byte_stream: ByteStream
+    ):
         """
         Starts tasks to process and send audio and text streams.
 
@@ -92,10 +98,7 @@ class WebsocketOutputStream:
         while True:
             data = await input_stream.get()
             if data is None:
-                json_data = {
-                    "type": "audio_end",
-                    "timestamp": time.time()
-                }
+                json_data = {"type": "audio_end", "timestamp": time.time()}
                 await self.ws.send_json(json_data)
             elif isinstance(data, bytes):
                 output_bytes_io = io.BytesIO()
@@ -106,19 +109,10 @@ class WebsocketOutputStream:
                 in_memory_wav.writeframes(data)
                 output_bytes_io.seek(0)
                 data = output_bytes_io.read()
-                json_data = {
-                    "type": "audio",
-                    "data": base64.b64encode(data).decode(),
-                    "timestamp": time.time()
-                }
+                json_data = {"type": "audio", "data": base64.b64encode(data).decode(), "timestamp": time.time()}
                 await self.ws.send_json(json_data)
             elif isinstance(data, str):
-                json_data = {
-                    "type": "message",
-                    "data": data,
-                    "timestamp": time.time()
-                }
+                json_data = {"type": "message", "data": data, "timestamp": time.time()}
                 await self.ws.send_json(json_data)
             else:
                 raise ValueError(f"Unsupported data type: {type(data)}")
-

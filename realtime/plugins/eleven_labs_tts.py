44146af2b5dd56f789fa535103febbcf7b23b720
Janak <janak2@users.noreply.github.com>
2024-08-29 00:23:32 -0700

    Update eleven_labs_tts.py

--- realtime/plugins/eleven_labs_tts.py
+++ realtime/plugins/eleven_labs_tts.py
@@ -158,6 +158,7 @@ class ElevenLabsTTS(Plugin):
                     # Finalize the audio generation
                     tracing.register_event(tracing.Event.TTS_END)
                     tracing.register_metric(tracing.Metric.TTS_TOTAL_BYTES, len(audio_byte_data))
+                    tracing.log_current_stats()
                     self.output_queue.put_nowait(None)
                     self._generating = False
 

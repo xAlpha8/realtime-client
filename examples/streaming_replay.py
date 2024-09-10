fdc53eb54da8a45da43135b192701c2cb964c63a
Sahil Manocha <sahilmanocha1994@gmail.com>
2024-09-09 23:40:20 -0700

    fixes

--- examples/streaming_replay.py
+++ examples/streaming_replay.py
@@ -17,7 +17,7 @@ class ReplayBot:
 
     @rt.streaming_endpoint()
     async def run(self, audio_input_queue: rt.AudioStream, video_input_queue: rt.VideoStream, text_input_queue: rt.TextStream):
-        text_stream_with_notif = rt.map(text_input_queue, lambda x: { "text": x, "notification": "<div>Hello</div>"})
+        text_stream_with_notif = rt.map(text_input_queue, lambda x: { "text": x, "render": "<div>Hello</div>"})
         text_stream_output = rt.map(text_stream_with_notif, lambda x: json.dumps(x))
         return (audio_input_queue, video_input_queue, text_stream_output)
 
